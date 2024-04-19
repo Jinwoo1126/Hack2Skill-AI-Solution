67ddddd2b7d63a8e3471a06f9dc12c569018c7c8
Jinwoo <jinwoo1126@lgensol.com>
2024-04-19 10:47:12 +0900

    update rm bg

--- main.py
+++ main.py
@@ -11,23 +11,29 @@ API_KEYS = {
     'SERP_API': os.environ.get('SERP_API')
 }
 
-keyword = 'chair', 'bed', 
+# load config
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-shopping_results = get_shopping_data(query=keyword, api_key=API_KEYS['SERP_API'])
+# get the keyword
+for keyword in config['list_of_furnitures']:
+    shopping_results = get_shopping_data(query=keyword, api_key=API_KEYS['SERP_API'])
 
-if not os.path.exists('data'):
-    os.makedirs('data')
+    if not os.path.exists('data'):
+        os.makedirs('data')
 
-for shopping_result in shopping_results:
-    shopping_result = get_thumbnail(shopping_dict=shopping_result, keywords=keyword)
+    for shopping_result in shopping_results:
+        shopping_result = get_thumbnail(shopping_dict=shopping_result, keywords=keyword)
 
-# save the shopping results to a JSON file
-with open('data/'+ keyword +'_shopping_results.json', 'w') as f:
-    json.dump(shopping_results, f, indent=4)
+    # save the shopping results to a JSON file
+    with open('data/'+ keyword +'_shopping_results.json', 'w') as f:
+        json.dump(shopping_results, f, indent=4)
 
 # ikea_dataset 
+        
+'''
 ikea_meta = get_ikea_data('data/ikea')
 with open('data/ikea/ikea_results.json', 'w') as f:
     json.dump(ikea_meta, f, indent=4)
-
+'''
     
