53df81c6e3cd6b83f7e4eed8aa489a68c4b45124
eumhwa <dmaghk1007@naver.com>
2024-04-26 02:38:59 +0000

    modify img2img api request

--- streamlit/diffusion_utils.py
+++ streamlit/diffusion_utils.py
@@ -8,7 +8,6 @@ import base64
 
 import requests
 
-# docker_net = "172.17.0.5:8000"
 docker_net = os.environ.get('DOCKER_NET', '172.17.0.5:8000')
 device = 'cpu'
 
@@ -60,20 +59,20 @@ def Paint_by_Example_API(base_image, mask_image, example_image):
     return converted_img
 
 
-def IMG2IMG_API(base_image, prompt):
-    ## fake api 
-    ## need to modify 
-    test_inference_url = f"http://{docker_net}/inference/test"
+def IMG2IMG_API(base_image:bytes, prompt:str, negative_prompt:str=''):
+    inference_url = f"http://{docker_net}/inference/img2img"
 
     print("api_call start")
     IMG_SIZE = (512, 512)
     
-    data = {}
-    
     encoded_init_bytes = base64.b64encode(base_image)
+    
+    data = {} 
     data['base_bytes'] = encoded_init_bytes.decode('ascii')
+    data['prompt'] = prompt
+    data['negative_prompt'] = negative_prompt
 
-    res = requests.post(test_inference_url, data=json.dumps(data))
+    res = requests.post(inference_url, data=json.dumps(data))
 
     decoded = base64.b64decode(res.content)
     converted_img = Image.open(io.BytesIO(decoded))
